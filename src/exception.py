6999195250ff93f5627356552dc45c32e765a8d0
Polina Sushko <polss2004@gmail.com>
2024-10-26 12:59:39 +0300

    EDA of a dataset

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
 import logging
+from src.logger import logging
+
 
 def error_message_detail(error, error_detail : sys):
     """
@@ -46,5 +48,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
-    
